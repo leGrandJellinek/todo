892b56d7cf2c977bf70c5188ad9d9b31ad187482
Jellinek <jellinekbraus@gmail.com>
2022-04-05 18:16:15 +0500

    end

---
+++ script.js
@@ -0,0 +1,3 @@
+let input = document.querySelector('input'),list = document.querySelector('ol');  
+input.addEventListener('keydown',function(el){if(el.keyCode === 13){if(input.value != ''){todo()}}else{''}})
+function todo(){let li = document.createElement('li');li.textContent = input.value;li.style = `color:white;font-size: 30px;`;input.value = '';list.append(li);}
